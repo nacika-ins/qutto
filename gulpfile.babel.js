bdc142eb83ac4a3099ba4e5714edd6ed220b39ba
toyama satoshi <toyamarinyon@gmail.com>
2015-10-14 20:38:23 +0900

    Refactoring gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,17 +1,30 @@
 import gulp from 'gulp'
 import babel from 'gulp-babel'
+import install from 'gulp-install'
+import del from 'del'
 import zip from 'gulp-zip'
 
-gulp.task('default', () => {
-  gulp
-    .src('./src/**/*.js')
-    .pipe(babel())
-    .pipe(gulp.dest('./'))
+gulp.task('clean', () => {
+  del(['dist', 'dist.zip'])
 })
 
-gulp.task('build', () => {
+gulp.task('compile', () => {
   gulp
-    .src(['./index.js','./node_modules/md5hex.js'])
-    .pipe(zip('qutto.zip'))
+    .src('src/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('dist/'))
+})
+
+gulp.task('node-modules', () => {
+  gulp
+    .src('package.json')
+    .pipe(gulp.dest('dist/'))
+    .pipe(install({production: true}))
+})
+
+gulp.task('zip', () => {
+  gulp
+    .src(['dist/**/*', '!dist/package.json'])
+    .pipe(zip('dist.zip'))
     .pipe(gulp.dest('./'))
 })
